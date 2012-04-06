ea82a2f2f80b7b4cbe098b3490d4b89397d3ad80
Ben Allfree <ben@benallfree.com>
2012-04-06 10:37:16 -0700

    Godegen locking

--- modules/activerecord/codegen.php
+++ modules/activerecord/codegen.php
@@ -4,7 +4,7 @@ $res = query_assoc("show variables where variable_name = 'ft_min_word_len'");
 if ($res[0]['Value']> $config['ft_min_word_len']) wax_error("mySQL FullText searching error. Set ft_min_word_len >= {$config['ft_min_word_len']}. Currently set to: ". $res[0]['Value']);
 
 $cg = new ArCodeGenerator($config, dirname(__FILE__), CACHE_FPATH."/activerecord");
-$md5 = $cg->calc_hash().time();
+$md5 = $cg->calc_hash();
 
 clear_cache(CACHE_FPATH."/activerecord");
 $cg->generate();
