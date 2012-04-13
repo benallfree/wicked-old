243b7f16595d5b0c278adf66b414de1f7e3d5a54
Ben Allfree <ben@benallfree.com>
2012-04-13 08:37:04 -0700

    Support for variables in flash()

--- modules/theme/lib/flash.php
+++ modules/theme/lib/flash.php
@@ -1,13 +1,17 @@
 <?
 
 
-function flash_next($msg)
+function flash_next()
 {
+  $args = func_get_args();
+  $msg = call_user_func_array('flash_interpolate', $args);
   $_SESSION['flash_next'][] = $msg;
 }
 
-function flash($msg)
+function flash()
 {
+  $args = func_get_args();
+  $msg = call_user_func_array('flash_interpolate', $args);
   $_SESSION['flash'][] = $msg;
 }
 
@@ -21,4 +25,15 @@ function get_flash()
   $flash = $_SESSION['flash'];
   $_SESSION['flash'] = array();
   return $flash;
+}
+
+function flash_interpolate()
+{
+  $msg = func_get_arg(0);
+  for($i=1;$i<func_num_args();$i++)
+  {
+    $v = func_get_arg($i);
+    $msg = preg_replace("/\?/", $v, $msg);
+  }
+  return $msg;
 }
