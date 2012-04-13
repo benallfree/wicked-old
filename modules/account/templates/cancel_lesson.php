bc2aede4ec4310f5a380428bf1a16a24a83c4bf1
Ben Allfree <ben@benallfree.com>
2012-04-13 08:40:08 -0700

    Template updates

---
+++ modules/account/templates/cancel_lesson.php
@@ -0,0 +1,8 @@
+Lesson cancellation - sorry! :(
+Your live private lesson via Skype video chat has been tragically canceled.
+
+Lesson Date & Time: <?=$time?> 
+Status: CANCELED
+Reason: Schedule Change
+
+Your lesson credit has been refunded and its expiration date has been reset. Please reschedule using the scheduling tool at http://coaching.benallfree.com/my/schedule to reschedule for another time.
--- modules/account/templates/unused_credits.php
+++ modules/account/templates/unused_credits.php
@@ -1,4 +1,8 @@
-You private lesson credits will expire soon
-You have private lesson credits waiting for you, but you have not scheduled them yet.
+Don't forget to schedule your lesson :)
+You have credits for live private lessons, but you haven't scheduled a lesson.
 
-They will expire soon. Go schedule them at http://coaching.benallfree.com/my/schedule.
+Go schedule them at http://coaching.benallfree.com/my/schedule
+
+Lessons work on a "lesson credit" system. You can apply your lesson credits toward any time slot. You can stack them, spread them out, skip a week, or double down. One person even did a stack of 4 (i.e., 1 hour 20 minutes!).
+
+Free live private lessons are my way of saying thanks for helping to build the pirate ship. If you have trouble booking a time or can't find a time that works, please contact me.
