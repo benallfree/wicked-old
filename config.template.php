d1e91e5b69ed440df5f5c913e703a7c2cec1751b
Ben Allfree <ben@benallfree.com>
2012-05-11 11:12:21 -0700

    Config template

---
+++ config.template.php
@@ -0,0 +1,25 @@
+<?
+
+$config = array(
+  /*
+  The default route and module to execute if one is not specified.
+  */
+  'default_module'=>'home',
+  'default_route'=>'index',
+  
+  /*
+  Which theme to use by default
+  */
+  'theme'=>'swatfame',
+  
+  /*
+  The app title appearing in the page and browser window.
+  */
+  'app_title'=>'SwatFame',
+);
+
+/*
+The cache path. This can be out of the web root if you desire.
+*/
+define('CACHE_FPATH', dirname(__FILE__).'/cache');
+
